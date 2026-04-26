b1939f6a08114e54f8ac3d9901511c5346564314
NazDevDsoul <arilfairuz371@gmail.com>
2026-04-26 23:50:24 +0700

    website sman 1 krembung jurnalistik

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Togle class active
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+//klik luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target)&& !navbarNav.contains(e.targe)){
+        navbarNav.classList.remove('active');
+    }
+})
